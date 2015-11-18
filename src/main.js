6f30ee5c0325801817ced5ac9110e44a14d68cd3
liuhongqiang <liuhongqiang@live.cn>
2015-11-18 20:35:50 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 var head = document.getElementsByTagName('head').item(0);
 
 var mainScript = document.createElement("script");
-mainScript.src = "http://files.cnblogs.com/files/smallyard/tieba-highlight.js";
+mainScript.src = "https://raw.githubusercontent.com/smallyard/tieba-highlight/master/src/tieba-highlight.js";
 head.appendChild(mainScript);
 
 var css = document.createElement("link");
-css.href = "http://files.cnblogs.com/files/smallyard/github.min.css";
+css.href = "http://apps.bdimg.com/libs/highlight.js/8.9.1/styles/github.min.css";
 css.rel = "stylesheet";
 head.appendChild(css);
--- src/tieba-highlight.js
+++ src/tieba-highlight.js
@@ -1,19 +1,35 @@
-$(function() {
-    function loadAsyncScript(src, callback) {
-        var head = document.getElementsByTagName("head")[0];
-        var script = document.createElement("script");
-        script.setAttribute("type", "text/javascript");
-        script.setAttribute("src", src);
-        script.setAttribute("async", true);
-        script.setAttribute("defer", true);
-        head.appendChild(script);
-        script.onload = function() {
-            callback();
-        };
+$(function () {
+    function loadAsyncScript(srcArr, callback) {
+        var totalCount = srcArr.length;
+        var readyCount = 0;
+        srcArr.forEach(function (src) {
+            var head = document.getElementsByTagName("head")[0];
+            var script = document.createElement("script");
+            script.setAttribute("type", "text/javascript");
+            script.setAttribute("src", src);
+            script.setAttribute("async", true);
+            script.setAttribute("defer", true);
+            head.appendChild(script);
+            script.onload = function () {
+                readyCount++;
+                if (totalCount == readyCount) {
+                    callback();
+                }
+            };
+        });
     }
-    loadAsyncScript("http://files.cnblogs.com/files/smallyard/highlight.min.js",
-    function() {
-        $('.d_post_content').each(function(i, block) {
+
+    var srcArr = [
+        "http://apps.bdimg.com/libs/highlight.js/8.9.1/highlight.min.js",
+        "http://tool.oschina.net/js/jsbeautify.js"
+    ];
+
+    loadAsyncScript(srcArr, function () {
+        $('.d_post_content').each(function (i, block) {
+            var text = $(block).text().replace(/^\s+/, '');
+            var beautifyText = js_beautify(text, 4, " ");
+            var beautifyHtml = beautifyText.replace(/\n/g, "<br/>").replace(/\s/g, '&nbsp;');
+            $(block).html(beautifyHtml);
             hljs.highlightBlock(block);
         });
     });
