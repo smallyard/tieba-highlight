205bc73387d8bffd08a7e9b9f0b3877828e8b1dd
liuhongqiang <liuhongqiang@live.cn>
2015-11-18 20:48:46 +0800

    remove jsbeautiful

--- src/tieba-highlight.js
+++ src/tieba-highlight.js
@@ -20,16 +20,11 @@ $(function () {
     }
 
     var srcArr = [
-        "http://apps.bdimg.com/libs/highlight.js/8.9.1/highlight.min.js",
-        "http://tool.oschina.net/js/jsbeautify.js"
+        "http://apps.bdimg.com/libs/highlight.js/8.9.1/highlight.min.js"
     ];
 
     loadAsyncScript(srcArr, function () {
         $('.d_post_content').each(function (i, block) {
-            var text = $(block).text().replace(/^\s+/, '');
-            var beautifyText = js_beautify(text, 4, " ");
-            var beautifyHtml = beautifyText.replace(/\n/g, "<br/>").replace(/\s/g, '&nbsp;');
-            $(block).html(beautifyHtml);
             hljs.highlightBlock(block);
         });
     });
